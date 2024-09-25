881407a8ac170de9885d3388c2ccb5535ca6819d
Alister Burt <alisterburt@gmail.com>
2024-09-25 09:11:16 -0700

    corrections and minor changes - time for ayse to construct orientations :-)

--- src/morphosamplers/samplers/path_samplers/pose_sampler_rings.py
+++ src/morphosamplers/samplers/path_samplers/pose_sampler_rings.py
@@ -9,61 +9,76 @@ from morphosamplers.utils import coaxial_y_vectors_from_z_vectors
 
 class RingPoseSampler(MorphoSampler):
     spacing: float
-    points_on_ring: int
+    n_points_per_ring: int
     radius: float
 
     def sample(self, obj: Path) -> PoseSet:
         # sample equidistant points along path (same as parallel path)
+        # these will be the positions of the rings
         from .point_sampler import PointSampler
         tck, total_length = PointSampler.prepare_spline(obj)
-        n_samples = int(total_length // self.spacing)
+        n_path_samples = int(total_length // self.spacing)
         max_u = 1 - ((total_length % self.spacing) / total_length)
-        u = np.linspace(0, max_u, num=n_samples)
-        positions = einops.rearrange(splev(u, tck), 'xyz b -> b xyz')
+        u = np.linspace(0, max_u, num=n_path_samples)
+        path_sample_positions = einops.rearrange(splev(u, tck), 'xyz b -> b xyz')
 
-        # direction of spline at the position
+
+        # Construct a coordinate system at each sampled point along the path
+        # - z points along the path
+        # - y is perpendicular to z
+        # - x is perpendicular to y and z
+
+        # z in our coordinate system is the direction of spline at the sample position
         # (derivative of the spline at the position)
-        tangents = splev(u, tck, der=1)
-        tangents = np.array(tangents).T
-        tangents /= np.linalg.norm(tangents, axis=1, keepdims=True)
-        # local coordinate system at the position (for circle points)
-        # (normal vectors to the direction at the position)
-        normals = np.cross(tangents, np.array([0, 0, 1]))
-        normals /= np.linalg.norm(normals, axis=1, keepdims=True)
-        binormals = np.cross(tangents, normals)
-        normals = normals[:, np.newaxis, :] # reshape (a, 3) to (a, 1, 3)
-        binormals = binormals[:, np.newaxis, :] # reshape (a, 3) to (a, 1, 3)
+        z_vectors = splev(u, tck, der=1)  # list of 3 (n, ) arrays
+        z_vectors = einops.rearrange(z_vectors, 'xyz b -> b xyz')
+        z_vectors /= np.linalg.norm(z_vectors, axis=1, keepdims=True)
 
-        # circle points
-        # circle angles based on how many points on ring
-        theta = np.linspace(0, 2 * np.pi, self.points_on_ring, endpoint=False)
-        # circles local coordinates based on the angles
-        cos_theta = np.cos(theta)[:, np.newaxis] # reshape (b,) to (b, 1)
-        sin_theta = np.sin(theta)[:, np.newaxis] # reshape (b,) to (b, 1)
+        # Construct y in our coordinate system, this is a vector
+        # perpendicular to our z-vectors.
+        # We do this by taking the cross product with a random vector.
+        # Find rows where the cross product is zero (i.e., the vectors are parallel or identical)
+        # and replace with a vector perpendicular to the random vector used in cross product
+        # calculation above then normalise
+        y_vectors = np.cross(z_vectors, np.array([0, 0, 1]))
+        idx_zero = np.linalg.norm(y_vectors, axis=1) == 0
+        y_vectors[idx_zero] = np.array([1, 0, 0])
+        y_vectors /= np.linalg.norm(y_vectors, axis=1, keepdims=True)
 
-        circle_points = (sin_theta * normals + cos_theta * binormals)
-        # scale circle points by radius
-        circle_points *= self.radius
-        # take each ring to its corresponding position on the spline
-        ring_positions = positions[:, np.newaxis, :] + circle_points
-        ring_positions = ring_positions.reshape(-1, 3) # reshape back to (a*b, 3)
+        # Finally, construct x in our coordinate system
+        x_vectors = np.cross(y_vectors, z_vectors)
 
-        orientations = np.empty((ring_positions.shape[0], 3, 3))
-        # generate orientations based on the backbone positions and circle positions
-        # (associate each circle point to its corresponding backbone here)
-        for i, (pos, ring_pos) in enumerate(zip(positions.repeat(self.points_on_ring, axis=0), ring_positions)):
-            # outward directing vector: vector pointing from the backbone to the ring position
-            new_z = ring_pos - pos
-            new_z /= np.linalg.norm(new_z)
-            # rest of the new coordinate system (vectors orthogonal to the new z)
-            new_x = np.cross(new_z, tangents[i // self.points_on_ring]) # tangent at the corresponding backbone position
-            new_x /= np.linalg.norm(new_x)
-            new_y = np.cross(new_z, new_x)
+        # Construct (n_path_samples, 3, 3) rotation matrices from x, y and z vectors
+        orientations = np.empty(shape=(n_path_samples, 3, 3))
+        orientations[:, :, 0] = x_vectors
+        orientations[:, :, 1] = y_vectors
+        orientations[:, :, 2] = z_vectors
 
-            # orientation matrix for each circle point
-            # local coordinate system at each circle point with z vector pointing outwards
-            orientations[i, :, 0] = new_x
-            orientations[i, :, 1] = new_y
-            orientations[i, :, 2] = new_z
+        # Generate points on a circle in the xy-place for each ring
+        theta = np.linspace(0, 2 * np.pi, self.n_points_per_ring, endpoint=False)
+        circle_x = np.cos(theta)
+        circle_y = np.sin(theta)
+        circle_z = np.zeros_like(theta)
+        circle_xyz = einops.rearrange([circle_x, circle_y, circle_z], 'xyz b -> b xyz')
+        circle_xyz *= self.radius  # (n_points_per_ring, 3)
+
+        # Rotate each point in the circle by each rotation
+        # target shape (n_points_per_ring, n_path_samples, 3)
+        orientations = einops.rearrange(orientations, 'n_path_samples i j -> 1 n_path_samples i j')
+        circle_xyz = einops.rearrange(circle_xyz, 'n_points_per_ring xyz -> n_points_per_ring 1 xyz 1')
+        rotated_circle_xyz = orientations @ circle_xyz  # (n_points_per_ring, n_path_samples, 3, 1)
+        rotated_circle_xyz = einops.rearrange(rotated_circle_xyz, 'b1 b2 i 1 -> b1 b2 i')
+
+        # place points around each path sample
+        final_positions = rotated_circle_xyz + path_sample_positions  # (n_points_per_ring, n_path_samples, 3)
+
+        ## ayse's job - construct the orientations for each particle
+        # finding vector to corrsponding path sample for each particle
+        # - z vector pointing perpendicular to path
+        # - y vector pointing along path
+        # - x vector perpendicular to both
+        # construct (n_points_per_ring, n_path_samples, 3, 3) orientations
+
+
 
         return PoseSet(positions=ring_positions, orientations=orientations)
